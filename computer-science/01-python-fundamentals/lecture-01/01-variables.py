170f21862d4dc7e054ad6e14bfee0e4602b5d1e5
anoushka <theolivequeen.xyz@gmail.com>
2024-08-17 13:01:16 +0300

    delete

--- computer-science/01-python-fundamentals/lecture-01/01-variables.py
+++
@@ -1,5 +0,0 @@
-name = "anoushka" # string
-school = "camp half-blood" # string
-grade = 10 # integer
-age = 16 # integer
-print(name, school, grade, age)
--- computer-science/01-python-fundamentals/lecture-01/03-type-casting.py
+++
@@ -1,9 +0,0 @@
-a = str(1.2)
-b = str(2.5)
-c = str(3.9)
-d = str(4.9)
-
-w = int(1.2)
-x = int(2.5)
-y = int(3.9)
-z = int(4.9)
