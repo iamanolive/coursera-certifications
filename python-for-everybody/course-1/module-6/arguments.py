2f79e4037be5d63e2d4897836d7bf369df761599
iamanolive <theolivequeen.xyz@gmail.com>
2024-09-04 15:47:47 +0300

    python module 6

---
+++ python-for-everybody/course-1/module-6/arguments.py
@@ -0,0 +1,21 @@
+# largest value letter
+big = max("hello world")
+print(big)
+
+# smallest value letter
+tiny = min("hello world")
+print(tiny)
+
+# type conversions
+print(float(99) / 100)
+i = 42; print(type(i))
+f = float(i)
+print(f); print(type(f))
+print(1 + 2 * float(3) / 4 - 5)
+
+# string conversions
+sval = "123"
+print(type(sval))
+ival = int(sval)
+print(type(ival))
+print(ival + 1)
---
+++ python-for-everybody/course-1/module-6/stored-steps.py
@@ -0,0 +1,7 @@
+def thing():
+    print("hello")
+    print("fun")
+
+thing() # invoke function
+print("zip")
+thing() # invoke function
