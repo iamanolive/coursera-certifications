85bb70bed9715ae71fa1735765018d5aa16c1253
iamanolive <theolivequeen.xyz@gmail.com>
2024-09-06 14:33:21 +0300

    python module 5

---
+++ python-for-everybody/course-2/module-5/dict-literals.py
@@ -0,0 +1,8 @@
+jjj = {
+    "chuck": 1,
+    "fred": 42,
+    "jan": 100
+}
+
+ooo = { } # empty dictionary
+print(ooo)
---
+++ python-for-everybody/course-2/module-5/dictionaries.py
@@ -0,0 +1,11 @@
+cabinet = dict()
+
+cabinet["summer"] = 12
+cabinet["fall"] = 3
+cabinet["spring"] = 75
+
+print(cabinet)
+print(cabinet["fall"])
+
+cabinet["fall"] = cabinet["fall"] + 2
+print(cabinet)
