093179e7157cfb452bd5bc73b10de56a1c69ce85
iamanolive <theolivequeen.xyz@gmail.com>
2024-09-07 14:27:39 +0300

    first turtle

---
+++ python-3-programming/course-1/module-1/turtle-graphics/first-turtle/turtle-rectangle.py
@@ -0,0 +1,12 @@
+import turtle
+
+wn = turtle.Screen()
+alex = turtle.Turtle()
+
+alex.forward(150) # draw length
+alex.left(90) # turn 90 degrees
+alex.forward(75) # draw width
+alex.left(90) # turn 90 degrees
+alex.forward(150) # draw length
+alex.left(90) # turn 90 degrees
+alex.forward(75) # draw width
