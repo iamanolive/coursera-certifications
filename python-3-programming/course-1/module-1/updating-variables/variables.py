66ccf103db837999f2ce92e882c78dc3927fdfb2
iamanolive <theolivequeen.xyz@gmail.com>
2024-09-07 13:05:27 +0300

    updating variables

---
+++ python-3-programming/course-1/module-1/updating-variables/variables.py
@@ -0,0 +1,19 @@
+x = 6; print(x)
+x = x + 1; print(x)
+
+# compound assignment
+x = 6; print(x) # initialize x
+x += 3; print(x) # increment x by 3
+x -= 1; print(x) # decrement x by 1
+
+
+s = 1; print(s) # increment x by 1
+s += 2; print(s) # increment x by 2
+s += 3; print(s) # increment x by 3
+s += 4; print(s) # increment x by 4
+s += 5; print(s) # increment x by 5
+s += 6; print(s) # increment x by 6
+s += 7; print(s) # increment x by 7
+s += 8; print(s) # increment x by 8
+s += 9; print(s) # increment x by 9
+s += 10; print(s) # increment x by 10
