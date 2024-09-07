d8d6e53f3c6513927aa1cd5f74f91a93b0d2fbad
iamanolive <theolivequeen.xyz@gmail.com>
2024-09-07 10:08:24 +0300

    operators and operands

---
+++ python-3-programming/course-1/module-1/operators-and-operands/operators.py
@@ -0,0 +1,12 @@
+print(100 + 200) # the addition operator
+print(5 - 2) # the subtraction operator
+print(2 * 4) # the multiplication operator
+print(10 / 3) # the division operator
+
+print(10 // 3) # truncated division
+print(10 % 3) # the modulo operator
+print(4 ** 2) # the exponentiation operator
+
+# calculating average
+print(10 + 20 / 2)
+print((10 + 20) / 2)
