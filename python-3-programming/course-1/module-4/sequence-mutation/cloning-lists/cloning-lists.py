3be359056e488f908b8f521ff2c67a3234d99c0d
iamanolive <theolivequeen.xyz@gmail.com>
2024-09-09 09:37:23 +0300

    sequence mutation

---
+++ python-3-programming/course-1/module-4/sequence-mutation/cloning-lists/cloning-lists.py
@@ -0,0 +1,8 @@
+a = [81, 82, 83]
+b = a[:]
+
+print(a == b)
+print(a is b)
+
+b[0] = 5
+print(a, b)
---
+++ python-3-programming/course-1/module-4/sequence-mutation/mutability/immutability.py
@@ -0,0 +1,11 @@
+greeting = "hello, world"
+new_greeting = "j" + greeting[1:]
+print(greeting, new_greeting)
+
+
+phrase = "many moons"
+phrase_expanded = phrase + " and many stars"
+phrase_larger = phrase_expanded + " litter"
+phrase_complete = "M" + phrase_larger[1:] + " the night sky."
+excited_phrase_complete = phrase_complete[:-1] + "!"
+print(excited_phrase_complete)
